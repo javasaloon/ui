722deafda367f750b4fe83acde2d5327db8220e6
Jacky Cheng <jacky.cheng@sap.com>
2014-11-19 10:18:55 +0800

    add

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,8 @@
-function showPic(whichpic) { 
+function showPic(whichpic) {
 	var placeholder = document.getElementById("placeholder");
-	if (!placeholder) return false; 
+	if (!placeholder) return false;
 	var source = whichpic.getAttribute("href");
-	placeholder.setAttribute("src", source); 
+	placeholder.setAttribute("src", source);
 
 	var description = document.getElementById("description");
 	if (!description) return false;
@@ -36,7 +36,38 @@ function prepareGallery() {
 			// Now if showPic is successful and returns true, we return false and the link won’t be followed by the browser.
 			// If showPic returns false then we assume that the image wasn't updated and return true to allow the default action to occur.
 			return !showPic(this);
-		}; 
+		};
+	}
+}
+// If these elements exist just to be manipulated by DOM methods, 
+// then it makes sense to also create them using DOM methods.
+function preparePlaceholder() {
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id", "placeholder");
+	placeholder.setAttribute("src", "images/placeholder.gif");
+	placeholder.setAttribute("alt", "my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id", "description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+
+	var gallery = document.getElementById("imagegallery");
+	gallery.parentNode.insertBefore(placeholder, gallery);
+	insertAfter(description, placeholder);
+} 
+
+// the DOM hasn’t provided a method called insertAfter
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling);
 	}
 }
 
@@ -52,4 +83,5 @@ function addLoadEvent(func) {
 	}
 }
 
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
